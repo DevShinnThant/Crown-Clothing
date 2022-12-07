10c891d2ba8802c2c78e9250b34f43d472b01ceb
shinnthant2004 <shinthantequi@gmail.com>
2022-12-07 18:47:09 +0630

    outlet & Route Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import {Route,Routes} from "react-router-dom";
+import Home from "./routes/home/home.component";
 
-import Home from "./routes/home.component";
 
-function App() {
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return (
+    <h1>Shop Page</h1>
+  )
+}
+
+const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   );
 }
