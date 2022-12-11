16e403000062592979c6425aeaccbd88489d844c
shinnthant2004 <shinthantequi@gmail.com>
2022-12-11 18:53:37 +0630

    google redirect

--- src/routes/sign-up/sign-up.component.jsx
+++ src/routes/sign-up/sign-up.component.jsx
@@ -1,14 +1,32 @@
-import { signInWithGooglePopUp,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { auth,signInWithGooglePopUp,signInWithGoogleRedirect,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignUp = () => {
+    
+    useEffect(()=>{
+      const getResult = async () => {
+        const response = await getRedirectResult(auth);
+        console.log(response.user);
+       };
+       getResult();
+    },[])
+
+
     const signInWithGoogle = async () => {
         const {user} = await signInWithGooglePopUp();
         await createUserDocumentFromAuth(user);
     };
+    const signInWithRedirect = async () => {
+       const {user} = await signInWithGoogleRedirect();
+       console.log(user);
+    }
     return (
         <div>
            <h2>Sign Up</h2>
-           <button onClick={signInWithGoogle}>Sign In With Google</button>
+           <button onClick={signInWithGoogle}>Sign In With Google Pop Up</button>
+           <button onClick={signInWithRedirect}>Sign In With Google Redirect</button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app';
 
-import {getAuth,GoogleAuthProvider,signInWithPopup} from 'firebase/auth';
+import {getAuth,GoogleAuthProvider,signInWithPopup,signInWithRedirect} from 'firebase/auth';
 
 import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore';
 
@@ -27,6 +27,8 @@ export const auth = getAuth();
 export const db = getFirestore();
 
 export const signInWithGooglePopUp = async () => await signInWithPopup(auth,provider);
+
+export const signInWithGoogleRedirect = async () => await signInWithRedirect(auth,provider);
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const docRef = doc(db,'users',userAuth.uid);
