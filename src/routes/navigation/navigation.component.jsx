10b08766bdec05191a5a5463b4cb0153072889a1
shinnthant2004 <shinthantequi@gmail.com>
2022-12-14 11:43:00 +0630

    cart icon component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,6 +9,7 @@ import { UserContext } from "../../contexts/user.context";
 import {ReactComponent as CrownSvg} from '../../assets/crown.svg';
 
 import './navigation.styles.scss';
+import CartIcon from "../../components/cart-icon/cart-icon.component.jsx";
 
 
 const Navigation = () => {
@@ -29,6 +30,7 @@ const Navigation = () => {
          {
           currentUser ? (<span className='nav-link' onClick={signOutHandler}>Sign Out</span>) : (<Link className="nav-link" to="/auth">Sign In</Link>)
          }
+         <CartIcon/>
         </div>
       </div>
       <Outlet/>
