c200da3caa1f89abe669d07fa8d6d4ad8bcdc7af
shinnthant2004 <shinthantequi@gmail.com>
2022-12-14 11:25:38 +0630

    shop page styling

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,21 @@
 import { useContext } from "react";
 
+import './shop.styles.scss';
+
+import ProductCard from "../../components/product-card/product-card.component";
+
 import { ProductsContext } from "../../contexts/products.context";
 
 const Shop = () => {
     const {products} = useContext(ProductsContext);
 
     return (
-        <div>
+        <div className="products-container">
         {
           products.map((product)=>{
-            return <h2>{product.name}</h2>
+            return (
+                <ProductCard key={product.id} product={product}/>
+            )
           })
         }
         </div>
