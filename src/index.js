ed559846536016a6b71bb7ae8cf4f286f052f7e4
shinnthant2004 <shinthantequi@gmail.com>
2022-12-17 14:05:42 +0630

    table hash data structure & optimization

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,37 @@
-import { useContext } from "react";
+import { useContext,Fragment } from "react";
 
 import './shop.styles.scss';
 
 import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from "../../contexts/products.context";
+import { CategoriesContext } from "../../contexts/categories.context";
 
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext);
 
     return (
-        <div className="products-container">
-        {
-          categoriesMap.map((product)=>{
-            return (
-                <ProductCard key={product.id} product={product}/>
-            )
-          })
-        }
-        </div>
+      <Fragment>
+      {
+         Object.keys(categoriesMap).map((title)=>{
+          return (
+            <Fragment key={title}>
+              <h2>{title}</h2>
+              <div className="products-container">
+              {
+                 categoriesMap[title].map((product)=>{
+                    return (
+                    <ProductCard key={product.id} product={product}/>
+                   )
+                  })
+              }
+              </div>
+            </Fragment>
+          )
+         })
+
+      }
+      </Fragment>
+        
     )
 }
 export default Shop;
