a41fefcb15ace116ec58ea7e51b3561813c64649
shinnthant2004 <shinthantequi@gmail.com>
2022-12-21 16:53:53 +0630

    categories reducer

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 
 import { addCollectionAndDocuments,getCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
@@ -7,8 +7,35 @@ export const CategoriesContext = createContext({
     setCategoriesMap: () => null
 });
 
+const CATEGORY_ACTION_TYPES = {
+  SET_CATEGORIES_MAP : 'SET_CATEGORIES_MAP'
+};
+
+const categoriesReducer = (state,action) => {
+  const {type,payload} = action;
+  
+  switch(type){
+    case 'SET_CATEGORIES_MAP' :
+      return {
+        ...state,
+        categoriesMap:payload
+      };
+    default : 
+      throw new Error('Unhandle type with category reducer')
+  }
+};
+
+const INITIAL_STATE = {
+  categoriesMap : [],
+};
+
+
 export const CategoriesProvider = ({children}) => {
-    const [categoriesMap,setCategoriesMap] = useState([]);
+    const [{categoriesMap},dispatch] = useReducer(categoriesReducer,INITIAL_STATE)
+ 
+    const setCategoriesMap = (categories) => {
+      dispatch({type:CATEGORY_ACTION_TYPES.SET_CATEGORIES_MAP,payload:categories})
+    };
 
     const value = {categoriesMap,setCategoriesMap};
 
