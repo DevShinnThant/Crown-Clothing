659bb3f724c2709049c512d22c756cc0ff68d9a3
shinnthant2004 <shinthantequi@gmail.com>
2022-12-26 09:02:57 +0630

    custom middleware

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,21 @@ import { compose,applyMiddleware } from "redux";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleWare = (store) => (next) => (action) => {
+    if(!action.type){
+        return next()
+    };
+
+    console.log('type: ',action.type);
+    console.log('payload: ',action.payload);
+    console.log('current State: ',store.getState());
+
+    next(action);
+
+    console.log('next state: ',store.getState());
+};
+
+const middleWares = [loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
