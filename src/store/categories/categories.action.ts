72c038a7171d9fae51c58869b01558996900fa50
shinnthant2004 <shinthantequi@gmail.com>
2023-01-24 22:09:54 +0630

    categories Action type

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,30 @@
+import {
+  Action,
+  ActionWithPayload,
+  createAction,
+} from "../../utils/firebase/reducer/reducer.utils";
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+
+export type FetchCategorySuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export const fetchCategoriesStart = (): FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (
+  categories: Category[]
+): FetchCategorySuccess =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
