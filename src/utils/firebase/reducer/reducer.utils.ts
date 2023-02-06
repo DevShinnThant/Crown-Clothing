d2a8996338e88389a4df2b5f0e3436111a460348
shinnthant2004 <shinthantequi@gmail.com>
2023-02-06 18:09:01 +0630

    actionMatcher helper function extension

--- src/utils/firebase/reducer/reducer.utils.ts
+++ src/utils/firebase/reducer/reducer.utils.ts
@@ -14,6 +14,24 @@ export type MatchAble<AC extends () => AnyAction> = AC & {
   match(action: AnyAction): action is ReturnType<AC>;
 };
 
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): MatchAble<AC>;
+
+export function withMatcher<
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): MatchAble<AC>;
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
 export function createAction<T extends string, P>(
   type: T,
   payload: P
